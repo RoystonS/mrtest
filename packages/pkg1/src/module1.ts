b5e46a75f06d1bcb4fae66543eaee2e1222c40ea
Royston Shufflebotham <royston@shufflebotham.org>
2020-10-04 13:20:46 +0100

    Initial revision

---
+++ packages/pkg1/src/module1.ts
@@ -0,0 +1,5 @@
+console.log("pkg1.module1 loading");
+
+export function run() {
+  console.log("pkg1.module1 running");
+}
---
+++ packages/pkg2/src/index.ts
@@ -0,0 +1,5 @@
+import { run } from '@my/pkg1';
+
+console.log("pkg2.index loading");
+
+run();

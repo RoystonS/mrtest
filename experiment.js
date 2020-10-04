70ebcfac37a03f9a4cf1fa34eb63e16c08b5245c
Royston Shufflebotham <royston@shufflebotham.org>
2020-10-04 14:39:38 +0100

    Extract config from Danger

--- experiment.js
+++ experiment.js
@@ -1,14 +1,4 @@
-const getCustomAppAuthFromEnv = () => {
-    const appID = process.env.DANGER_GITHUB_APP_ID || process.env.PERIL_INTEGRATION_ID
-    const key = process.env.DANGER_GITHUB_APP_PRIVATE_SIGNING_KEY || process.env.PRIVATE_GITHUB_SIGNING_KEY
-    const installID = process.env.DANGER_GITHUB_APP_INSTALL_ID || process.env.PERIL_ORG_INSTALLATION_ID
-  
-    return {
-      appID,
-      key,
-      installID,
-    }
-  }
+const {getAuthWhenUsingDangerJSApp, getCustomAppAuthFromEnv} = require('danger/distribution/platforms/github/comms/checksCommenter');
 
 const custom = process.env.DANGER_JS_APP_INSTALL_ID ? getAuthWhenUsingDangerJSApp() : getCustomAppAuthFromEnv();
 
